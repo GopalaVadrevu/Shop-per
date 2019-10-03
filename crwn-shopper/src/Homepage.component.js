7860578f89d7eeef9b6c0e8df36543680e7b2e76
Gopala Krishna Vadrevu <44609621+VadrevuGopala@users.noreply.github.com>
2019-10-03 16:51:07 +0530

    initial framework crown shopper

---
+++ crwn-shopper/src/Homepage.component.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import './Homepage.styles.scss';
+
+const HomePage =() =>(
+
+    <div className='homepage' >
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='item-content'>
+                    <h1 className='item-title'>HATS</h1>
+                    <span className='item-subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='item-content'>
+                    <h1 className='item-title'>JACKTES</h1>
+                    <span className='item-subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='item-content'>
+                    <h1 className='item-title'>SHOES</h1>
+                    <span className='item-subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+            <div className='item-content'>
+                <h1 className='item-title'>WOMENS</h1>
+                <span className='item-subtitle'>Shop Now</span>
+            </div>
+            </div>
+            <div className='menu-item'>
+                <div className='item-content'>
+                    <h1 className='item-title'>MENS</h1>
+                    <span className='item-subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+)
+export default HomePage;
