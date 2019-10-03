06e196cd5ae01044f80b7a5ac3d3a579437d784c
Gopala Krishna Vadrevu <44609621+VadrevuGopala@users.noreply.github.com>
2019-10-03 20:06:38 +0530

    added styling elements for overflow

--- crwn-shopper/src/components/menu-item/menu-item.component.js
+++ crwn-shopper/src/components/menu-item/menu-item.component.js
@@ -5,13 +5,17 @@ import './menu-item.styles.scss';
 const MenuItem =({title,imageUrl,size}) =>(
 
     <div
-        style ={{
-            backgroundImage:`url(${imageUrl})`
-        }}
         className={`menu-item ${size}`}
     >
+        <div
+            className='background-image'
+            style ={{
+                backgroundImage:`url(${imageUrl})`
+            }}
+
+        />
         <div className='item-content'>
-            <h1 className='item-title'>{title}</h1>
+            <h1 className='item-title'>{title.toUpperCase()}</h1>
             <span className='item-subtitle'>Shop Now</span>
         </div>
     </div>
