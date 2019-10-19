8931c8b4733e5da67b88b35505c09f97ccc999c5
Gopala Krishna Vadrevu <44609621+VadrevuGopala@users.noreply.github.com>
2019-10-20 04:19:00 +0530

    updated shot data with new data. Updated cart.utils to add the multiple entities and quantity

--- crwn-shopper/src/components/Collection-items/Collection-item.component.js
+++ crwn-shopper/src/components/Collection-items/Collection-item.component.js
@@ -2,27 +2,34 @@ import React from 'react';
 
 import './Collection-item.styles.scss';
 import CustomButton from '../custome-button/custom-button.component';
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
 
-const CollectionItem =({id, name, price, imageUrl}) => (
+const CollectionItem =({item, addItem}) => (
 
     <div className='collection-item'>
 
         <div
             className ='image'
             style={{
-                backgroundImage: `url(${imageUrl})`
+                backgroundImage: `url(${item.imageUrl})`
             }}
             />
 
         <div className='collection-footer' >
-            <span className='name' >{name}</span>
-            <span className='price' >${price}</span>
+            <span className='name' >{item.name}</span>
+            <span className='price' >${item.price}</span>
         </div>
-        <CustomButton>Add to Cart</CustomButton>
+        <CustomButton onClick={()=> addItem(item)} inverted>Add to Cart</CustomButton>
     </div>
 
 
 
 )
 
-export default CollectionItem;
+const mapDispatchToProps  = dispatch => ({
+    addItem: item =>dispatch(addItem(item))
+
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
